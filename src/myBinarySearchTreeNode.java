218ea9867ac1e19bff7596c426040c0f28d5fe36
Alexis Rodriguez <ralexis1092@gmail.com>
2020-04-14 22:40:58 -0600

    Finished the Constructor methods, as well as the insert and height methods. Added javadoc documentation to all the methods and created the tester java file

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -2,46 +2,123 @@ class myBinarySearchTreeNode{
   int myValue;
   myBinarySearchTreeNode left;
   myBinarySearchTreeNode right;
-    
+
+  /**
+   * Created a new node with empty child pointers
+   *
+   * @param inValue the value of the new node
+   */
   myBinarySearchTreeNode(int inValue){
-    // created a new node with empty child pointers
+
+    this.myValue = inValue;
+    this.left = null;
+    this.right = null;
 
   }
-  
+
+  /**
+   * Creates a new Binary Search Tree rooted at the first value in the array
+   * by inserting elements into the tree in the order they are given in A.
+   *
+   * @param A an array that will be used to create a binary search tree
+   */
+
   myBinarySearchTreeNode(int[] A){
-    // creates a new Binary Search Tree rooted at the first value in the array
-    /// by inserting elements into the tree in the order they are given in A.
+
+    this.myValue = A[0];
+    this.left = null;
+    this.right = null;
+    for(int i = 1; i < A.length; i++){
+      insert(A[i]);
+    }
 
   }
-  
+
+  /**
+   * This method takes a new integer as input and decides
+   *    if it should be place:
+   *       * as the left child,
+   *       * as the right child,
+   *       * in the left subtree,
+   *       * or in the right subtree.
+   *    If the value already exists in the tree, no action is taken.
+   *
+   * @param inValue the value that will be added as a new node in the binary search tree
+   */
   public void insert(int inValue){
-    // This method takes a new integer as input and decides 
-    // if it should be place: 
-    //    * as the left child,
-    //    * as the right child, 
-    //    * in the left subtree,
-    //    * or in the right subtree.
-    // If the value already exists in the tree, no action is taken. 
+
+    if(inValue > this.myValue){
+      if(this.right == null){
+        this.right = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        this.right.insert(inValue);
+      }
+    }
+    else if(inValue < this.myValue){
+      if(this.left == null){
+        this.left = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        this.left.insert(inValue);
+      }
+    }
+    else{
+      System.out.println("Error: Value " + inValue +" is a duplicate value");
+    }
     
   }
-  
+
+  /**
+   * This method recursively calculates the height of the entire (sub)tree.
+   * This method will take O(n) time
+   *
+   * @return The height of the root node
+   */
   public int height(){
-     // This method recursively calculates the height of the entire (sub)tree.
-     // This method will take O(n) time
-     return -1;
+    int left = 0;
+    int right = 0;
+    if(this.right == null && this.left == null){
+      return 0;
+    }
+    if(this.left != null){
+      left = 1 + this.left.height();
+    }
+    if(this.right != null){
+      right = 1 + this.right.height();
+    }
+    if(left>right){
+      return left;
+    }
+    else{
+      return right;
+    }
   }
-  
+
+  /**
+   * This method recursively calculates the depth of a given search value.
+   * If the given value is not in the tree, this method returns -1.
+   * Note that if the tree is a proper BST, this method should complete in O(log n) time.
+   * Additionally, remember that the depth is the number of nodes on the path from a node to the root
+   * (i.e. the number of the recursive calls).
+   *
+   * @param search the value that will be searched on the binary tree search
+   * @return the depth of the node of the value given
+   */
   public int depth(int search){
-     // This method recursively calculates the depth of a given search value. 
-     // If the given value is not in the tree, this method returns -1. 
-     // Note that if the tree is a proper BST, this method should complete in O(log n) time.
-     // Additionally, remember that the depth is the number of nodes on the path from a node to the root 
-     // (i.e. the number of the recursie calls).
+
     return -1;
   }
-  
-  // Utility function included so you can debug your solution. 
+
+  /**
+   * Utility function included so you can debug your solution.
+   */
   public void print() { print(""); }
+
+  /**
+   * helper method for the print method
+   * @param prefix
+   */
   private void print(String prefix) {
     System.out.println(prefix + myValue);
     prefix = prefix.replace('\u251C', '\u2502');
---
+++ src/myBinarySearchTreeNodeTester.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class myBinarySearchTreeNodeTester {
+
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+       myBinarySearchTreeNode a = new myBinarySearchTreeNode(5);
+       a.insert(3);
+       a.insert(10);
+       a.insert(7);
+       a.insert(12);
+       a.insert(5);
+       a.insert(2);
+       a.print();
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert1() {
+        int[] hello = {5,1,2,3,4,5,6,9,8,10};
+        myBinarySearchTreeNode a = new myBinarySearchTreeNode(hello);
+        a.print();
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert2() {
+        int[] hello = {5,1,2,3,4,11,7,8,10,6,12,13,14,15,16};
+        myBinarySearchTreeNode a = new myBinarySearchTreeNode(hello);
+        a.print();
+        System.out.println(a.height());
+    }
+
+}
